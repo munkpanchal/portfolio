e1ef5316ee7826e45a71972f4de97eedfb736a94
Mayank Panchal <mayank@screetract.com>
2024-09-17 07:14:48 +0530

    Minor changes fixed

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,12 +1,12 @@
 <footer class="footer">
     <div class="container footer-wrapper py-4">
         <div class="footer-top">
-            <h2 class="text-center text-8xl capitalize">
+            <h2 class="text-center text-3xl sm:text-8xl capitalize">
                 lets connect..
             </h2>
             <div class="social flex justify-center">
                 <span>
-                    <a href="">LINKEDIN
+                    <a href="https://www.linkedin.com/in/mayankpanchal01/" target="_blank">LINKEDIN
                     </a>
                 </span>
             </div>
